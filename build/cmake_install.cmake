f7be9e304a77ac3381ec57b99fc89b407bcc9733
ZHANG <1450678447@qq.com>
2021-08-03 11:00:55 +0800

    add tf package

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -127,6 +127,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/sklir/ZHANGYan/ros_tutorial/build/gtest/cmake_install.cmake")
   include("/home/sklir/ZHANGYan/ros_tutorial/build/meta_package/cmake_install.cmake")
+  include("/home/sklir/ZHANGYan/ros_tutorial/build/tf_package/cmake_install.cmake")
   include("/home/sklir/ZHANGYan/ros_tutorial/build/test_launch/cmake_install.cmake")
   include("/home/sklir/ZHANGYan/ros_tutorial/build/tutorial_beginner/cmake_install.cmake")
 
